b1d28b9303572771e3a72849bbff6b0e54c3bcf4
EllieMott <elizabethmott1326@gmail.com>
2024-11-03 22:58:03 -0500

    +media queries in stated and root files

--- script.js
+++ script.js
@@ -9,19 +9,12 @@ function ready(){
     let heartButton = document.getElementsByClassName('heart-button')[0];
     heartButton.addEventListener('click', heartRedirect);
 
-    let loginButton = document.getElementsByClassName('login')[0];
-    loginButton.addEventListener('click', loginRedirect);
-
     let followButton = document.getElementsByClassName('followButton')[0];
     followButton.addEventListener('click', followRedirect)
 }
 
 function heartRedirect(){
     window.open('https://codepen.io/login?type=love', '_self');
-}
-
-function loginRedirect(){
-    window.open('https://codepen.io/login', '_self');
 }
 
 function followRedirect(){
